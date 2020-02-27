525e3b815700846f2aad42e2ee737328eb782f44
Wanqi-wq <1457208272@qq.com>
2020-02-27 23:25:53 +0800

    search

--- src/pages/search/Search.vue
+++ src/pages/search/Search.vue
@@ -1,13 +1,66 @@
 <template>
-  <div>搜索页面</div>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box class="searchbox" @query="inputChange" ref="searchbox"/>
+    </div>
+    
+    <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li class="item" v-for="(item,index) in hotList" @click="setQuery(item)" :key="index">
+                <span>{{item}}</span>
+              </li>
+            </ul>
+          </div>
+  </div>
 </template>
 
 <script>
+import SearchBox from './childComps/SearchBox'
+import { hotList } from 'common/config'
 export default {
-
+  name: 'Search',
+  data() {
+    return {
+      hotList,
+    }
+    
+  },
+  components: {
+    SearchBox
+  },
+  methods: {
+    inputChange() {
+      //当搜索框发生改变时，进行数据请求
+    },
+    setQuery(item) {
+      this.$refs.searchbox.setQuery(item)
+    }
+  },
+  created() {
+    console.log(hotList)
+  }
 }
 </script>
+<style lang="stylus" scoped>
+  @import "~assets/stylus/variable"
+.search
+  .search-box-wrapper
+    margin 20px
+  .hot-key
+    margin: 0 20px 20px 20px
+    .title
+      margin-bottom: 20px
+      font-size: $font-size-medium
+      color: $color-text-l
+    .item
+      display: inline-block
+      padding: 5px 10px
+      margin: 0 20px 10px 0
+      border-radius: 6px
+      background: $color-highlight-background
+      font-size: $font-size-medium
+      color: $color-text-d
 
-<style>
 
 </style>
---
+++ src/pages/search/childComps/SearchBox.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input ref="query" class="box" :placeholder="placeholder" v-model="query"/>
+    <i class="icon-dismiss" @click="clear" v-show="query"></i>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SearchBox',
+  data() {
+    return {
+      query:''
+    }
+  },
+  props: {
+    placeholder:{
+      type: String,
+      default: '请输入歌曲或歌手名称'
+    }
+  },
+  watch: {
+    query(newQuery) {
+      this.$emit('query', newQuery)
+    }
+  },
+  methods: {
+    clear() {
+      this.query = ''
+    },
+    setQuery(query) {
+      this.query = query
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~assets/stylus/variable"
+  .search-box
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    width: 100%
+    padding: 0 16px
+    height: 40px
+    background: $color-highlight-background
+    border-radius: 6px
+    .icon-search
+      font-size: 24px
+      color: $color-background
+    .box
+      flex: 1
+      margin: 0 5px
+      line-height: 18px
+      background: $color-highlight-background
+      color: $color-text
+      font-size: $font-size-medium
+      &::placeholder
+        color: $color-text-d
+    .icon-dismiss
+      font-size: 16px
+      color: $color-background
+
+</style>
